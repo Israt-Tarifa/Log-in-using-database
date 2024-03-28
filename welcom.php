1a5d5b44524fdff411720c6941acc9ef48a7428f
Israt-Tarifa <77012964+Israt-Tarifa@users.noreply.github.com>
2024-03-29 01:12:37 +0600

    Add files via upload

---
+++ welcom.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+
+        h1 {
+            color: #333;
+            margin-top: 50px;
+        }
+
+        h3 {
+            color: #555;
+            margin-top: 20px;
+        }
+
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <h1>Login Successful</h1>
+    <h3><a href="https://israt-tarifa.github.io/Tarifa_portfolio/">Visit My Portfolio</a></h3>
+</body>
+</html>
